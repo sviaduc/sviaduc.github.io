8a28d391d4cef8332931d1b1ccd5610dd93c6cb9
Stanton Viaduc <sjviaduc@gmail.com>
2019-11-23 16:21:05 -0700

    Menu overlay

--- front/js/main.js
+++ front/js/main.js
@@ -4,7 +4,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Inttial State Of Menu
 
